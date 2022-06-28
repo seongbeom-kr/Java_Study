a607e07095e253e864f5431331ad0e5fe967ab25
seongbeom lim <seongbeom1234@gmail.com>
2022-06-28 14:35:59 +0900

    Update BJ_2839.java

--- src/week02_Arrays/BJ_2839.java
+++ src/week02_Arrays/BJ_2839.java
@@ -1,10 +1,18 @@
 package week02_Arrays;
-
+import java.util.*;
 public class BJ_2839 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int min =0;
+		int a,b; // 변수설
+		min += n/5;
+		a=n%5;
+		min += a/3;
+		b=a%3;
+		if(b==0) System.out.println(min);
+		else System.out.println(-1);
 	}
 
 }

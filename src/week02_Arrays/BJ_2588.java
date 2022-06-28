5240364e6c471d890a947f37cf7c113131b06342
seongbeom lim <seongbeom1234@gmail.com>
2022-06-28 15:32:19 +0900

    Update BJ_2588.java

--- src/week02_Arrays/BJ_2588.java
+++ src/week02_Arrays/BJ_2588.java
@@ -1,12 +1,22 @@
 package week02_Arrays;
-import java.util.Scanner;
+
+import java.util.*;
+
+
 public class BJ_2588 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int b = sc.nextInt();
+		String B = sc.next();
+		char[] b = B.toCharArray();
+		
+		System.out.println(a*(b[2]-'0'));
+		System.out.println(a*(b[1]-'0'));
+		System.out.println(a*(b[0]-'0'));
+		System.out.println(a* (Integer.parseInt(B)));
+		
+		
 		
 	}
-
 }

fab8d3602b51c80d9439025b6e32d94725f65a96
seongbeom lim <seongbeom1234@gmail.com>
2022-06-28 15:41:00 +0900

    Update BJ_2495.java

--- src/week02_Arrays/BJ_2495.java
+++ src/week02_Arrays/BJ_2495.java
@@ -7,19 +7,27 @@ public class BJ_2495 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		char[] arr = sc.next().toCharArray();
-		int max = 0, cnt = 0;
-		for(int i=1; i<8;++i) {
-			if(arr[i-1]==arr[i]) {
-				cnt+=1;
-				max=cnt;
+		for (int i = 0; i < 3; ++i) {
+			String str = sc.next();
+			int cnt = 1, max = 1;
+			char prev = str.charAt(0);
+
+			for (int j = 1; j < 8; ++j) {
+				char now = str.charAt(j);
+				if (now == prev) {
+					cnt++;
+
+				} else {
+					max = Math.max(max, cnt);
+					cnt = 1;
+					prev = now;
+				}
+
+				max = Math.max(max, cnt);
 			}
-			else {
-				cnt=0;
-			}
-			max =Math.max(max, cnt);
+			System.out.println(max);
 		}
-		System.out.println(max);
+
 	}
 
 }

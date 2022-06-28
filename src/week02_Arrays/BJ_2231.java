acf27418b7df78fe848636035de43221d6395b04
seongbeom lim <seongbeom1234@gmail.com>
2022-06-28 14:47:10 +0900

    Update BJ_2231.java

--- src/week02_Arrays/BJ_2231.java
+++ src/week02_Arrays/BJ_2231.java
@@ -1,10 +1,28 @@
 package week02_Arrays;
+
 import java.util.*;
+
 public class BJ_2231 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			int number = i;
+			int sum = 0;
 
+			while (number != 0) {
+				sum += number % 10;
+				number /= 10;
+
+			}
+			if (sum + i == n) {
+				result = i;
+				break;
+			}
+		}
+		System.out.println(result);
 	}
 
 }

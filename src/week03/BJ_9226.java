4fbac04512aaf275caddb4998b29413adc26da79
seongbeom lim <seongbeom1234@gmail.com>
2022-07-12 22:45:07 +0900

    Update BJ_9226.java

--- src/week03/BJ_9226.java
+++ src/week03/BJ_9226.java
@@ -6,12 +6,28 @@ public class BJ_9226 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 		while (true) {
 			String input = sc.next();
-			if (input == "#")
+			String res = "";
+			boolean check = false;
+			if (input.equals("#"))
 				break;
-		
-		
+			for (int i = 0; i < input.length(); ++i) {
+				for (int j = 0; j < vowels.length; ++j) {
+					if (input.charAt(i) == vowels[j]) {
+						check = true;
+						break;
+					}
+
+				}
+
+				if (check) {
+					res = input.substring(i, input.length()) + input.substring(0, i);
+					break;
+				}
+			}
+			System.out.println(res + "ay");
 		}
 	}
 

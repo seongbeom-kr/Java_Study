13f86720046bb29efc08715cc9beb5493fe49e59
seongbeom lim <seongbeom1234@gmail.com>
2022-07-20 04:58:47 +0900

    Update BJ_1018.java

--- src/week04/BJ_1018.java
+++ src/week04/BJ_1018.java
@@ -1,10 +1,56 @@
 package week04;
+
 import java.util.*;
+
 public class BJ_1018 {
 
-	public static void main(String[] args) {
-		
+	static char chess[][];
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		chess = new char[N][M];
+		sc.nextLine();
+
+		for (int i = 0; i < N; i++) {
+			String s = sc.nextLine();
+			for (int j = 0; j < M; j++) {
+				chess[i][j] = s.charAt(j);
+			}
+		}
+
+		int ans = 1000000;
+
+		for (int i = 0; i < N - 7; i++) {
+			for (int j = 0; j < M - 7; j++) {
+				ans = Math.min(ans, f(i, j));
+			}
+		}
+		System.out.println(ans);
+	}
+
+	public static int f(int x, int y) {
+		int B = 0;
+		int W = 0;
+
+		for (int i = x; i < x + 8; i++) {
+			for (int j = y; j < y + 8; j++) {
+				if ((i + j) % 2 == 0) {
+					if (chess[i][j] == 'B')
+						W++;
+					else
+						B++;
+				} else {
+					if (chess[i][j] == 'W')
+						W++;
+					else
+						B++;
+				}
+			}
+		}
+		return Math.min(B, W);
 	}
 
 }
